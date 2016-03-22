479421a1dc6bd78402804275b3e3e1246ba5cd31
Eric <eric@ickyfoot.com>
2016-03-22 00:43:09 -0400

    - add *.db to .gitignore - push existing contents to remote repo

---
+++ engine.rb
@@ -0,0 +1,49 @@
+require_relative '../shared/people/person'
+
+class IOUtil
+	def initialize
+	end
+	
+	def collectInfo(greet=true)
+		if (greet)
+			puts "\nHello, and welcome!\n\n"\
+				"Please provide a little information to get started.\n\n"
+		end
+		puts "What's your first name?"
+		firstName = gets
+		firstName = firstName.chomp
+		
+		puts "And, your last name?"
+		lastName = gets
+		lastName = lastName.chomp
+		
+		puts "What about your birthday?"
+		birthday = gets
+		birthday = birthday.chomp
+		
+		puts "Please confirm the following information:"
+		puts "Name: "+firstName+" "+lastName
+		puts "Birthday: "+birthday
+		puts "Is this correct? Y/N"
+		
+		confirmed = gets
+		confirmed = confirmed.chop
+		
+		if (confirmed == "Y")
+			person = Person.new(firstName,lastName,birthday)
+			person.saveInfo
+			person.greet
+			person.calculateAge
+			person.getAge
+			puts person.countPeople
+		else
+			puts "OK, let's try again."
+			collectInfo(false)
+		end
+	end
+end
+
+if __FILE__ == $0
+	iou = IOUtil.new
+	iou.collectInfo
+end
